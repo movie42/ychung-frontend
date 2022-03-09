cef63d47488b61142f3dc3334e6db25c847e3fd3
ko Hyunsu <movie3342@gmail.com>
2022-03-09 11:50:58 +0900

    error : invaild hook ... normalize css나 styled component recoil과 같은 패키지를 설치 후에 root에서 사용하려고 하면 invaild hook... 에러가 발생하는데 해결이 되지 않는다.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+import { Normalize } from "styled-normalize";
+
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <Normalize />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root"),
 );
