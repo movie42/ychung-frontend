379baa28b755a88f23668836078d14e4e1cadc23
ko Hyunsu <movie3342@gmail.com>
2022-04-23 04:52:35 +0900

    feat:ListContainer, ListItem.tsx
    Notice, Blog에서 반복되는 리스트 컴포넌트를 추상화하였다.
    
    비슷한 스타일에는 사용할 수 있지만 그렇지 않은 스타일은 사용하기 어렵다. 확장성이 매우 떨어진다.

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 import styled from "styled-components";
