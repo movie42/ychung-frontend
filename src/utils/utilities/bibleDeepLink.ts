6dc95e6277ce1622ad039a51a5671cecf5f347db
ko Hyunsu <movie3342@gmail.com>
2022-04-25 16:05:38 +0900

    feat:WorshipDetail.tsx 딥 링크 기능 추가
    성경 링크를 클릭시 딥 링크로 갓피플 성경을 연결해 준다.
    WorshipDetail.tsx
    - 성경 링크 클릭시 사용자가 갓피플 성경으로 연결을 희망하면 갓피플 성경 어플리케이션을 열어준다.
    bibleDeepLink
    - godpeopleDeepLink() 갓피플 성경 deeplink를 생성해서 갓피플 성경을 열어준다.
    - checkGodpeopleBibleInstall() 갓피플 성경이 설치되어있지 않으면 설치 여부를 묻는다. 희망하지 않을 시에 웹으로 성경을 열어주냐고 한번 더 물어보고 이것도 희망하지 않으면 아무 동작을 하지 않는다.

---
+++ src/utils/utilities/bibleDeepLink.ts
@@ -0,0 +1,157 @@
+export const godpeopleDeepLink = (
+  word: string,
+  chapter: number,
+  verse: number
+) => {
+  const url = "gpbible://link";
+
+  const BIBLE_DATA_SET: { [key: string]: string } = {
+    gen: "01",
+    exo: "02",
+    lev: "03",
+    num: "04",
+    deu: "05",
+    jos: "06",
+    jdg: "07",
+    rut: "08",
+    "1sa": "09",
+    "2sa": "10",
+    "1ki": "11",
+    "2ki": "12",
+    "1ch": "13",
+    "2ch": "14",
+    ezr: "15",
+    neh: "16",
+    est: "17",
+    job: "18",
+    psa: "19",
+    pro: "20",
+    ecc: "21",
+    sng: "22",
+    isa: "23",
+    jer: "24",
+    lam: "25",
+    ezk: "26",
+    dan: "27",
+    hos: "28",
+    jol: "29",
+    amo: "30",
+    oba: "31",
+    jnh: "32",
+    mic: "33",
+    nam: "34",
+    hab: "35",
+    zep: "36",
+    hag: "37",
+    zec: "38",
+    mal: "39",
+    mat: "40",
+    mrk: "41",
+    luk: "42",
+    jhn: "43",
+    act: "44",
+    rom: "45",
+    "1co": "46",
+    "2co": "47",
+    gal: "48",
+    eph: "49",
+    php: "50",
+    col: "51",
+    "1th": "52",
+    "2th": "53",
+    "1ti": "54",
+    "2ti": "55",
+    tit: "56",
+    phm: "57",
+    heb: "58",
+    jas: "59",
+    "1pe": "60",
+    "2pe": "61",
+    "1jn": "62",
+    "2jn": "63",
+    "3jn": "64",
+    jud: "65",
+    rev: "66",
+  };
+
+  const chapterParser = (chapter: number): string => {
+    if (chapter < 10) {
+      return `00${chapter}`;
+    }
+    if (chapter < 100) {
+      return `0${chapter}`;
+    }
+    return `${chapter}`;
+  };
+
+  const verseParser = (verse: number): string => {
+    if (verse < 10) {
+      return `00${verse}`;
+    }
+
+    if (verse < 100) {
+      return `0${verse}`;
+    }
+
+    return `${verse}`;
+  };
+
+  window.location.replace(
+    `${url}?ujj=${BIBLE_DATA_SET[word]}${chapterParser(chapter)}${verseParser(
+      verse
+    )}`
+  );
+};
+
+export const createBibleLink = (
+  word: string,
+  chapter: number,
+  verse: number
+) => {
+  return `https://www.bskorea.or.kr/bible/korbibReadpage.php?version=SAENEW&book=${word}&chap=${chapter}&sec=${verse}`;
+};
+
+export const checkGodpeopleBibleInstall = (
+  word: string,
+  chapter: number,
+  verse: number
+) => {
+  const clearTimers = () => {
+    clearInterval(check);
+    clearTimeout(timer);
+  };
+
+  const isHideWeb = () => {
+    if (document.hidden) {
+      clearTimers();
+    }
+  };
+
+  const check = setInterval(isHideWeb, 200);
+
+  const timer = setTimeout(() => {
+    redirectStore();
+  }, 500);
+
+  const redirectStore = () => {
+    const ua = navigator.userAgent.toLocaleLowerCase();
+
+    if (ua.indexOf("android") === -1) {
+      if (window.confirm("웹으로 성경을 보여드려요?")) {
+        window.location.href = createBibleLink(word, chapter, verse);
+      }
+      return;
+    }
+
+    if (window.confirm("갓피플 성경을 설치하실래요? ")) {
+      window.location.href =
+        ua.indexOf("android") > -1
+          ? "https://play.google.com/store/apps/details?id=com.godpeople.GPBIBLE"
+          : "https://apps.apple.com/kr/app/갓피플성경/id511852665";
+    } else {
+      if (window.confirm("웹으로 성경을 보여드려요?")) {
+        window.location.href = createBibleLink(word, chapter, verse);
+      }
+    }
+  };
+};
