99cdda369116932f635f05764d14ee6ae5c016d3
A <movie3342@gmail.com>
2022-06-24 01:02:53 +0900

    주보 쓰기, 업데이트 컴포넌트 스타일 변경

--- src/page/Worship/WorshipCreate.tsx
+++ src/page/Worship/WorshipCreate.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
-import { AiOutlineCloudUpload } from "react-icons/ai";
+import { AiFillCaretDown, AiOutlineCloudUpload } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 
 import { BIBLE_DATA_SET } from "../../bible";
 import usePost from "../../utils/customhooks/usePost";
+import FormItem from "../../components/Form/FormItem";
+import Label from "../../components/Form/Label";
+import Input from "../../components/Form/Input";
+import Select from "../../components/Form/Select";
 
 const Wrapper = styled.div`
   margin-top: 8rem;
@@ -33,17 +37,45 @@ const Wrapper = styled.div`
   }
 `;
 
-const InputWrapper = styled.form`
+const Form = styled.form`
   box-sizing: border-box;
-  margin-bottom: 1rem;
+  .bible-address-container {
+    grid-template-columns: 1fr 1fr 1fr;
+    & {
+      div {
+        border: 0;
+        display: grid;
+        align-items: center;
+        grid-template-columns: 0.4fr 1fr;
+      }
+    }
+  }
+  .select-box {
+    position: relative;
+    select {
+      width: 100%;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      border: 0;
+      outline: unset;
+      font-size: 3rem;
+    }
+    span {
+      position: absolute;
+      z-index: 0;
+      top: 50%;
+      right: 2rem;
+      transform: translateY(-50%);
+    }
+  }
   div {
-    border-bottom: 1px solid ${(props) => props.theme.color.gray300};
     label {
       font-size: 2rem;
       font-weight: 700;
       margin-right: 0.5rem;
     }
-    input {
+    Input {
       width: 80%;
       box-sizing: border-box;
       padding: 0.8rem;
@@ -52,10 +84,15 @@ const InputWrapper = styled.form`
       font-size: 3rem;
       outline: 0;
     }
+    input[type="number"]::-webkit-outer-spin-button,
+    input[type="number"]::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
   }
 `;
 
-function WorshipCreate() {
+const WorshipCreate = () => {
   const navigate = useNavigate();
 
   const {
@@ -95,10 +132,10 @@ function WorshipCreate() {
       <button className="upload" onClick={onSubmit}>
         <AiOutlineCloudUpload />
       </button>
-      <InputWrapper onSubmit={onSubmit}>
-        <div>
-          <label htmlFor="title">제목</label>
-          <input
+      <Form onSubmit={onSubmit}>
+        <FormItem>
+          <Label htmlFor="title">제목</Label>
+          <Input
             id="title"
             type="text"
             placeholder="강론 제목이 무엇인가요?"
@@ -106,41 +143,55 @@ function WorshipCreate() {
               required: "강론 제목을 알려주세요.",
             })}
           />
-        </div>
+        </FormItem>
         {errors.title && <p>{errors?.title?.message}</p>}
-        <div>
-          <label htmlFor="word">본문</label>
-          <select id="word" {...register("word")}>
-            {paintObject().map((value) => value)}
-          </select>
-        </div>
+        <FormItem>
+          <Label htmlFor="word">본문</Label>
+          <div className="select-box">
+            <Select id="word" {...register("word")} options={paintObject()} />
+            <span>
+              <AiFillCaretDown />
+            </span>
+          </div>
+        </FormItem>
         {errors.word && <p>{errors?.word?.message}</p>}
-        <div>
-          <label htmlFor="chapter">장</label>
-          <input
-            id="chapter"
-            type="number"
-            {...register("chapter", {
-              required: "장을 입력하세요.",
-            })}
-          />
-        </div>
-        <div>
-          <label htmlFor="verse">시작 절</label>
-          <input
-            id="verse"
-            type="number"
-            {...register("verse", {
-              required: "절을 입력하세요.",
-            })}
-          />
-          <label htmlFor="verse_end">끝 절</label>
-          <input id="verse_end" type="number" {...register("verse_end")} />
-        </div>
+        <FormItem className="bible-address-container">
+          <div>
+            <Label htmlFor="chapter">장</Label>
+            <Input
+              id="chapter"
+              type="number"
+              placeholder="0"
+              {...register("chapter", {
+                required: "장을 입력하세요.",
+              })}
+            />
+          </div>
+          <div>
+            <Label htmlFor="verse">시작 절</Label>
+            <Input
+              id="verse"
+              type="number"
+              placeholder="0"
+              {...register("verse", {
+                required: "절을 입력하세요.",
+              })}
+            />
+          </div>
+          <div>
+            <Label htmlFor="verse_end">끝 절</Label>
+            <Input
+              id="verse_end"
+              placeholder="0"
+              type="number"
+              {...register("verse_end")}
+            />
+          </div>
+        </FormItem>
         {errors.verse_end && <p>{errors?.verse_end?.message}</p>}
-        <div>
-          <label htmlFor="pastor">강론</label>
-          <input
+        <FormItem>
+          <Label htmlFor="pastor">강론</Label>
+          <Input
             id="pastor"
             type="text"
             defaultValue="김상돈"
@@ -148,10 +199,21 @@ function WorshipCreate() {
               required: "강론을 누가 하는지 알려주세요.",
             })}
           />
-        </div>
-        <div>
-          <label htmlFor="worshipTeam">찬양</label>
-          <input
+        </FormItem>
+        <FormItem>
+          <Label htmlFor="reader">성경봉독</Label>
+          <Input
+            id="reader"
+            type="text"
+            defaultValue="다같이"
+            {...register("reader", {
+              required: "누가 성경을 읽나요?",
+            })}
+          />
+        </FormItem>
+        <FormItem>
+          <Label htmlFor="worshipTeam">찬양</Label>
+          <Input
             id="worshipTeam"
             type="text"
             placeholder="찬양 팀이 누구인가요?"
@@ -160,10 +222,10 @@ function WorshipCreate() {
               required: "찬양 팀을 알려주세요.",
             })}
           />
-        </div>
-        <div>
-          <label htmlFor="prayer">대표기도</label>
-          <input
+        </FormItem>
+        <FormItem>
+          <Label htmlFor="prayer">대표기도</Label>
+          <Input
             id="prayer"
             type="text"
             placeholder="대표를 누구하나요?"
@@ -171,10 +233,10 @@ function WorshipCreate() {
               required: "대표기도를 누가하는지 알려주세요.",
             })}
           />
-        </div>
-        <div>
-          <label htmlFor="advertisement">광고</label>
-          <input
+        </FormItem>
+        <FormItem>
+          <Label htmlFor="advertisement">광고</Label>
+          <Input
             id="advertisement"
             type="text"
             placeholder="광고를 누가 하나요?"
@@ -183,10 +245,22 @@ function WorshipCreate() {
               required: "누가 광고를 하는지 알려주세요.",
             })}
           />
-        </div>
-      </InputWrapper>
+        </FormItem>
+        <FormItem>
+          <Label htmlFor="benediction">봉헌 및 축도</Label>
+          <Input
+            id="benediction"
+            type="text"
+            placeholder="봉헌, 축도 기도를 누가 하나요?"
+            defaultValue="김상돈"
+            {...register("benediction", {
+              required: "봉헌, 축도 기도자를 알려주세요.",
+            })}
+          />
+        </FormItem>
+      </Form>
     </Wrapper>
   );
-}
+};
 
 export default WorshipCreate;
--- src/page/Worship/WorshipUpdate.tsx
+++ src/page/Worship/WorshipUpdate.tsx
@@ -2,19 +2,21 @@ import React, { useEffect } from "react";
 
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
-import { AiOutlineCloudUpload } from "react-icons/ai";
+import { AiFillCaretDown, AiOutlineCloudUpload } from "react-icons/ai";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { BIBLE_DATA_SET } from "../../bible";
 import usePost from "../../utils/customhooks/usePost";
 import { IWorshipItems } from "../../state/worship.atom";
-import InputStyle from "../../components/Form/Input";
-import LabelStyle from "../../components/Form/Label";
+import Input from "../../components/Form/Input";
+import Label from "../../components/Form/Label";
+import FormItem from "../../components/Form/FormItem";
+import Select from "../../components/Form/Select";
 
 const Wrapper = styled.div`
   margin-top: 8rem;
   position: relative;
-  height: 70vh;
+  height: 100vh;
 
   button.upload {
     position: absolute;
@@ -39,7 +41,57 @@ const Wrapper = styled.div`
 
 const Form = styled.form`
   box-sizing: border-box;
-  margin-bottom: 1rem;
+  .bible-address-container {
+    grid-template-columns: 1fr 1fr 1fr;
+    & {
+      div {
+        border: 0;
+        display: grid;
+        align-items: center;
+        grid-template-columns: 0.4fr 1fr;
+      }
+    }
+  }
+  .select-box {
+    position: relative;
+    select {
+      width: 100%;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      border: 0;
+      outline: unset;
+      font-size: 3rem;
+    }
+    span {
+      position: absolute;
+      z-index: 0;
+      top: 50%;
+      right: 2rem;
+      transform: translateY(-50%);
+    }
+  }
+  div {
+    label {
+      font-size: 2rem;
+      font-weight: 700;
+      margin-right: 0.5rem;
+    }
+    Input {
+      width: 80%;
+      box-sizing: border-box;
+      padding: 0.8rem;
+      border: 0;
+      text-align: left;
+      font-size: 3rem;
+      outline: 0;
+    }
+    input[type="number"]::-webkit-outer-spin-button,
+    input[type="number"]::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+  }
 `;
 
 const FormItemContainer = styled.div`
@@ -59,9 +111,6 @@ const FormItemContainer = styled.div`
     outline: 0;
   }
 `;
-
-const Input = styled(InputStyle)``;
-const Label = styled(LabelStyle)``;
 
 interface IWorshipUpdate {
   data?: IWorshipItems;
@@ -105,48 +154,72 @@ const WorshipUpdate = ({ data }: IWorshipUpdate) => {
         <AiOutlineCloudUpload />
       </button>
       <Form onSubmit={onSubmit}>
-        <FormItemContainer>
+        <FormItem>
           <Label htmlFor="title">제목</Label>
           <Input
             id="title"
             type="text"
-            placeholder="강론 제목이 무엇인가요?"
             defaultValue={data?.title}
-            {...register("title", { required: "제목이 반드시 필요합니다." })}
+            placeholder="강론 제목이 무엇인가요?"
+            {...register("title", {
+              required: "강론 제목을 알려주세요.",
+            })}
           />
-        </FormItemContainer>
+        </FormItem>
         {errors.title && <p>{errors?.title?.message}</p>}
-        <FormItemContainer>
+        <FormItem>
           <Label htmlFor="word">본문</Label>
-          <select id="word" {...register("word")} defaultValue={data?.word}>
-            {paintObject().map((label, value) => (
-              <option value={value}>{label}</option>
-            ))}
-          </select>
-        </FormItemContainer>
+          <div className="select-box">
+            <Select
+              id="word"
+              {...register("word")}
+              defaultValue={data?.word}
+              options={paintObject()}
+            />
+            <span>
+              <AiFillCaretDown />
+            </span>
+          </div>
+        </FormItem>
         {errors.word && <p>{errors?.word?.message}</p>}
-        <FormItemContainer>
-          <Label htmlFor="chapter">장</Label>
-          <Input
-            id="chapter"
-            type="number"
-            defaultValue={data?.chapter}
-            {...register("chapter", { required: "장을 입력하세요." })}
-          />
-        </FormItemContainer>
-        <FormItemContainer>
-          <Label htmlFor="verse">시작 절</Label>
-          <Input
-            id="verse"
-            type="number"
-            defaultValue={data?.verse}
-            {...register("verse", { required: "절을 입력하세요." })}
-          />
-          <Label htmlFor="verse_end">끝 절</Label>
-          <Input id="verse_end" type="number" {...register("verse_end")} />
-        </FormItemContainer>
+        <FormItem className="bible-address-container">
+          <div>
+            <Label htmlFor="chapter">장</Label>
+            <Input
+              id="chapter"
+              type="number"
+              placeholder="0"
+              defaultValue={data?.chapter}
+              {...register("chapter", {
+                required: "장을 입력하세요.",
+              })}
+            />
+          </div>
+          <div>
+            <Label htmlFor="verse">시작 절</Label>
+            <Input
+              id="verse"
+              type="number"
+              placeholder="0"
+              defaultValue={data?.verse}
+              {...register("verse", {
+                required: "절을 입력하세요.",
+              })}
+            />
+          </div>
+          <div>
+            <Label htmlFor="verse_end">끝 절</Label>
+            <Input
+              id="verse_end"
+              placeholder="0"
+              type="number"
+              defaultValue={data?.verse_end}
+              {...register("verse_end")}
+            />
+          </div>
+        </FormItem>
         {errors.verse_end && <p>{errors?.verse_end?.message}</p>}
-        <FormItemContainer>
+        <FormItem>
           <Label htmlFor="pastor">강론</Label>
           <Input
             id="pastor"
@@ -156,18 +229,32 @@ const WorshipUpdate = ({ data }: IWorshipUpdate) => {
               required: "강론을 누가 하는지 알려주세요.",
             })}
           />
-        </FormItemContainer>
-        <FormItemContainer>
+        </FormItem>
+        <FormItem>
+          <Label htmlFor="reader">성경봉독</Label>
+          <Input
+            id="reader"
+            type="text"
+            placeholder="누가 성경을 읽는지 알려주세요."
+            defaultValue={data?.reader || "다같이"}
+            {...register("reader", {
+              required: "누가 성경을 읽나요?",
+            })}
+          />
+        </FormItem>
+        <FormItem>
           <Label htmlFor="worshipTeam">찬양</Label>
           <Input
             id="worshipTeam"
             type="text"
             placeholder="찬양 팀이 누구인가요?"
             defaultValue={data?.worshipTeam}
-            {...register("worshipTeam", { required: "찬양 팀을 알려주세요." })}
+            {...register("worshipTeam", {
+              required: "찬양 팀을 알려주세요.",
+            })}
           />
-        </FormItemContainer>
-        <FormItemContainer>
+        </FormItem>
+        <FormItem>
           <Label htmlFor="prayer">대표기도</Label>
           <Input
             id="prayer"
@@ -178,8 +265,8 @@ const WorshipUpdate = ({ data }: IWorshipUpdate) => {
               required: "대표기도를 누가하는지 알려주세요.",
             })}
           />
-        </FormItemContainer>
-        <FormItemContainer>
+        </FormItem>
+        <FormItem>
           <Label htmlFor="advertisement">광고</Label>
           <Input
             id="advertisement"
@@ -190,7 +277,19 @@ const WorshipUpdate = ({ data }: IWorshipUpdate) => {
               required: "누가 광고를 하는지 알려주세요.",
             })}
           />
-        </FormItemContainer>
+        </FormItem>
+        <FormItem>
+          <Label htmlFor="benediction">봉헌 및 축도</Label>
+          <Input
+            id="benediction"
+            type="text"
+            placeholder="봉헌, 축도 기도를 누가 하나요?"
+            defaultValue={data?.benediction || "김상돈"}
+            {...register("benediction", {
+              required: "봉헌, 축도 기도자를 알려주세요.",
+            })}
+          />
+        </FormItem>
       </Form>
     </Wrapper>
   );
