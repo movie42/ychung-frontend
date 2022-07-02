62ca6c29c9d6ad95877331d5eafad6fd7bc64eec
A <movie3342@gmail.com>
2022-07-03 02:03:25 +0900

    주보에 교육 정보 컴포넌트  추가

--- client/src/page/Educations/index.ts
+++ client/src/page/Educations/index.ts
@@ -7,4 +7,3 @@ export {
 } from "./Detail";
 export { EducationsUpdate, Group, GroupContainer, Human } from "./UpdateGroup";
 export { Educations, GroupItem, GroupItemContainer } from "./UserPage";
-export { ReportsContainer } from "./Leader";
--- client/src/page/Root/Join.tsx
+++ client/src/page/Root/Join.tsx
@@ -1,24 +1,8 @@
-import usePostOrPatch from "@/utils/hooks/usePost";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router";
-import styled from "styled-components";
 import Button from "../../components/Buttons/Button";
 import FormItem from "../../components/Form/FormItem";
 import SEO from "../../components/SEO/SEO";
-
-const Wrapper = styled.div`
-  margin-top: 8rem;
-`;
-const MessageContainer = styled.div`
-  margin-top: -8rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100vh;
-`;
 
 interface SubmitProps {
   email: string;
@@ -29,19 +13,14 @@ interface SubmitProps {
 }
 
 function Join() {
-  const navigate = useNavigate();
+  const [csrfToken, setCsrfToken] = useState("");
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     setError,
   } = useForm<SubmitProps>();
-
-  const { mutate: joinMutate, isSuccess } = usePostOrPatch({
-    url: "/api/join",
-    queryKey: "join",
-    method: "POST",
-  });
 
   const onSubmit = handleSubmit(async (data: SubmitProps) => {
     if (data.password !== data.password2) {
@@ -52,122 +31,134 @@ function Join() {
         },
         { shouldFocus: true }
       );
-      return;
     }
 
-    joinMutate(data);
+    const response = await fetch(`/join`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": csrfToken,
+      },
+      body: JSON.stringify({ ...data }),
+      credentials: "include",
+      mode: "cors",
+    });
+
+    if (response.ok) {
+      window.location.pathname = "/";
+    }
   });
 
-  useEffect(() => {
-    if (isSuccess) {
-      const timeout = setTimeout(() => navigate("/login"), 3000);
-      return () => clearTimeout(timeout);
-    }
-  }, [isSuccess]);
+  const csrf = async () => {
+    const response = await fetch(`/getCSRFToken`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      mode: "cors",
+    });
+    const parseResponse = await response.json();
 
-  return isSuccess ? (
-    <Wrapper>
-      <MessageContainer>
-        <h1>가입하신것을 축하드립니다! 😙</h1>
-        <p>로그인 화면으로 이동합니다.</p>
-      </MessageContainer>
-    </Wrapper>
-  ) : (
+    setCsrfToken(parseResponse.CSRFToken);
+  };
+
+  useEffect(() => {
+    csrf();
+  }, []);
+
+  return (
     <>
       <SEO title="회원가입" keywords="회원가입" />
-      <Wrapper>
-        <h1>회원 가입</h1>
-        <form onSubmit={onSubmit}>
-          <FormItem>
-            <label>이메일</label>
-            <input
-              type="text"
-              {...register("email", {
-                required: "이메일을 입력해야합니다.",
-                pattern: {
-                  value:
-                    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                  message: "이메일이 아닙니다.",
+      <h1>회원 가입</h1>
+      <form onSubmit={onSubmit}>
+        <FormItem>
+          <label>이메일</label>
+          <input
+            type="text"
+            {...register("email", {
+              required: "이메일을 입력해야합니다.",
+              pattern: {
+                value:
+                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: "이메일이 아닙니다.",
+              },
+              validate: {
+                checkValue: async (value) => {
+                  const dbEmail = await fetch(`/api/checked-db/email=${value}`);
+                  return dbEmail.ok ? true : "이미 가입된 사용자입니다.";
                 },
-                validate: {
-                  checkValue: async (value) => {
-                    const dbEmail = await fetch(
-                      `/api/checked-db/email=${value}`
-                    );
-                    return dbEmail.ok ? true : "이미 가입된 사용자입니다.";
-                  },
+              },
+            })}
+          />
+        </FormItem>
+        <p>{errors?.email?.message}</p>
+        <FormItem>
+          <label>사용자 이름</label>
+          <input
+            type="text"
+            {...register("userName", {
+              required: "사용자 이름을 입력해야합니다.",
+              pattern: {
+                value: /^[a-zA-Z0-9]{5,10}$/,
+                message:
+                  "사용자 이름은 영문과 숫자조합으로만 이루어져야하며 5자 이상 10자 이하여야합니다.",
+              },
+              validate: {
+                checkValue: async (value) => {
+                  const dbEmail = await fetch(
+                    `/api/checked-db/userName=${value}`
+                  );
+                  return dbEmail.ok ? true : "이미 가입된 사용자입니다.";
                 },
-              })}
-            />
-          </FormItem>
-          <p>{errors?.email?.message}</p>
-          <FormItem>
-            <label>사용자 이름</label>
-            <input
-              type="text"
-              {...register("userName", {
-                required: "사용자 이름을 입력해야합니다.",
-                pattern: {
-                  value: /^[a-zA-Z0-9]{5,10}$/,
-                  message:
-                    "사용자 이름은 영문과 숫자조합으로만 이루어져야하며 5자 이상 10자 이하여야합니다.",
-                },
-                validate: {
-                  checkValue: async (value) => {
-                    const dbEmail = await fetch(
-                      `/api/checked-db/userName=${value}`
-                    );
-                    return dbEmail.ok ? true : "이미 가입된 사용자입니다.";
-                  },
-                },
-              })}
-            />
-          </FormItem>
-          <p>{errors?.userName?.message}</p>
-          <FormItem>
-            <label>실명</label>
-            <input
-              type="text"
-              {...register("name", {
-                required: "실명을 입력해야합니다.",
-                pattern: {
-                  value: /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{2,6}$/,
-                  message: "한글 2글자 이상 6글자 이하로 실명을 입력해주세요. ",
-                },
-              })}
-            />
-          </FormItem>
-          <p>{errors?.name?.message}</p>
-          <FormItem>
-            <label>비밀번호</label>
-            <input
-              type="password"
-              {...register("password", {
-                required: "비밀번호를 입력하세요.",
-                pattern: {
-                  value:
-                    /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,}$/,
-                  message:
-                    "비밀번호는 영문, 특수문자, 숫자 조합으로만 이루어질 수 있습니다. 최소 8자 이상이어야합니다.",
-                },
-              })}
-            />
-          </FormItem>
-          <p>{errors?.password?.message}</p>
-          <FormItem>
-            <label>비밀번호 확인</label>
-            <input
-              id="password2"
-              type="password"
-              {...register("password2", {
-                required: "앞에서 입력한 비밀번호를 똑같이 입력해주세요.",
-              })}
-            />
-            <p>{errors?.password2?.message}</p>
-          </FormItem>
-          <Button buttonType="block">가입하기</Button>
-        </form>
-      </Wrapper>
+              },
+            })}
+          />
+        </FormItem>
+        <p>{errors?.userName?.message}</p>
+        <FormItem>
+          <label>실명</label>
+          <input
+            type="text"
+            {...register("name", {
+              required: "실명을 입력해야합니다.",
+              pattern: {
+                value: /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{2,6}$/,
+                message: "한글 2글자 이상 6글자 이하로 실명을 입력해주세요. ",
+              },
+            })}
+          />
+        </FormItem>
+        <p>{errors?.name?.message}</p>
+        <FormItem>
+          <label>비밀번호</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: "비밀번호를 입력하세요.",
+              pattern: {
+                value:
+                  /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,}$/,
+                message:
+                  "비밀번호는 영문, 특수문자, 숫자 조합으로만 이루어질 수 있습니다. 최소 8자 이상이어야합니다.",
+              },
+            })}
+          />
+        </FormItem>
+        <p>{errors?.password?.message}</p>
+        <FormItem>
+          <label>비밀번호 확인</label>
+          <input
+            id="password2"
+            type="password"
+            {...register("password2", {
+              required: "앞에서 입력한 비밀번호를 똑같이 입력해주세요.",
+            })}
+          />
+          <p>{errors?.password2?.message}</p>
+        </FormItem>
+        <Button buttonType="block">가입하기</Button>
+      </form>
     </>
   );
 }
