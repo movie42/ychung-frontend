2d4ccb8f1268555f356fa0e98666c27008108458
movie42 <movie3342@gmail.com>
2022-08-28 12:19:07 +0900

    Editor 이미지 업로드시 로딩바 css 변경

--- src/components/Editors/Editor.tsx
+++ src/components/Editors/Editor.tsx
@@ -4,25 +4,72 @@ import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-sy
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { api } from "@/lib/api";
 import { useMutation } from "react-query";
 import { AxiosError } from "axios";
+
+const loadingKeyframes = keyframes`
+  0%{
+    rotate: 0deg
+  }
+  
+  100%{
+    rotate: 360deg ;
+  }
+`;
+const opacityKeyframes = keyframes`
+  0% {
+    background-position:200% center;
+  }
+`;
 
 const Wrapper = styled.div`
   position: relative;
   height: 100%;
   .loading {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
     box-sizing: border-box;
     position: absolute;
-    top: 4.7rem;
-    left: 0;
-    padding: 0.8rem;
-    width: 100%;
+    top: 5.7rem;
+    right: 1rem;
     z-index: 1000;
-    font-size: 1.5rem;
-    color: ${(props) => props.theme.color.fontColorWhite};
-    background-color: ${(props) => props.theme.color.secondary400};
+    width: 100%;
+    height: 2rem;
+    .loading-title {
+      position: relative;
+      padding-top: 0.2rem;
+      margin-right: 1rem;
+      font-size: 1.5rem;
+      color: ${(props) => props.theme.color.gray400};
+      text-align: center;
+      background: linear-gradient(
+        to right,
+        #ff0000 5%,
+        #fff 20%,
+        #fff 80%,
+        #ff2200 95%
+      );
+      background-clip: text;
+      background-size: 200% auto;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      animation: ${opacityKeyframes} 2s linear infinite;
+    }
+
+    .loading-ball {
+      border: 0.5rem solid transparent;
+      background-image: linear-gradient(#fff, #fff),
+        linear-gradient(to right, white 0%, red 100%);
+      background-origin: border-box;
+      background-clip: content-box, border-box;
+      width: 1rem;
+      height: 1rem;
+      border-radius: 100%;
+      animation: ${loadingKeyframes} 2s linear infinite;
+    }
   }
   .toastui-editor-defaultUI {
     .ProseMirror {
@@ -64,7 +111,12 @@ function EditorContainer({ initialValue, reference }: IEditorContainerProps) {
   return (
     <>
       <Wrapper>
-        {isLoading && <p className="loading">이미지 업로드 중...</p>}
+        {isLoading && (
+          <span className="loading">
+            <span className="loading-title">이미지를 서버에 올리고 있어요</span>
+            <span className="loading-ball"></span>
+          </span>
+        )}
         <Editor
           ref={reference}
           customHTMLRenderer={{
