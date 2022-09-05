4388136ce177340801fa27217a9a026e89543cfa
A <movie3342@gmail.com>
2022-09-05 14:04:01 +0700

    fix : ListContainer 변경
    ListContainer
    - Skeleton 리스트는 Loading과 Refetching때 동작합니다.
    - Loading은 처음에 Refetching은 무한 스크롤 아이템이 새로 갱신되었을 때.

--- src/components/List/ListContainer.tsx
+++ src/components/List/ListContainer.tsx
@@ -2,6 +2,7 @@ import { AnimatePresence } from "framer-motion";
 import React, { ReactElement, useEffect, useState } from "react";
 import styled from "styled-components";
 import SkeletonForListContainer from "../Loading/Skeletons/SkeletonForListContainer";
+import SkeletonForListItem from "../Loading/Skeletons/SkeletonForListItem";
 
 const List = styled.ul`
   display: grid;
@@ -17,32 +18,25 @@ const List = styled.ul`
 interface IListComponentProps<T, V>
   extends React.HTMLAttributes<HTMLUListElement> {
   isLoading: boolean;
+  isRefetching: boolean;
   data: T[];
   renderFunc: (item: T) => React.ReactNode;
-  skeletonRenderFunc: (value: V, index: number, array: V[]) => React.ReactNode;
 }
 
 const ListContainer = <T, V extends unknown>({
   isLoading,
+  isRefetching,
   data,
   renderFunc,
-  skeletonRenderFunc,
 }: IListComponentProps<T, V>) => {
-  const [isSkeleton, setIsSkeleton] = useState(true);
-
-  useEffect(() => {
-    let timer: NodeJS.Timeout;
-    if (!isLoading) {
-      timer = setTimeout(() => setIsSkeleton(false), 1500);
-    }
-
-    return () => clearTimeout(timer);
-  }, [isLoading]);
-
-  return isSkeleton ? (
-    <SkeletonForListContainer amount={6} renderFunc={skeletonRenderFunc} />
-  ) : (
-    <List>{data.map(renderFunc)}</List>
+  return (
+    <List>
+      <>
+        {isLoading && <SkeletonForListItem />}
+        {data.map(renderFunc)}
+        {isRefetching && <SkeletonForListItem />}
+      </>
+    </List>
   );
 };
 
