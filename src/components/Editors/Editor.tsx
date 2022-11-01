2ca76b6849de7bb145d15209fcdcb1d07c94d268
movie42 <movie3342@gmail.com>
2022-11-01 18:24:57 +0900

    editor image 업로드 수정(서버문제였음)

--- src/components/Editors/Editor.tsx
+++ src/components/Editors/Editor.tsx
@@ -4,73 +4,15 @@ import "@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-sy
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 import colorSyntax from "@toast-ui/editor-plugin-color-syntax";
-import styled, { keyframes } from "styled-components";
-import { api } from "@/lib/api";
-import { useMutation } from "react-query";
-import { AxiosError } from "axios";
+import styled from "styled-components";
 
-const loadingKeyframes = keyframes`
-  0%{
-    rotate: 0deg
-  }
-  
-  100%{
-    rotate: 360deg ;
-  }
-`;
-const opacityKeyframes = keyframes`
-  0% {
-    background-position:200% center;
-  }
-`;
+import useImageUpload from "./hooks/useImageUpload";
+import SpiningLoading from "../Loading/SpiningLoading";
 
 const Wrapper = styled.div`
   position: relative;
   height: 100vh;
-  .loading {
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    box-sizing: border-box;
-    position: absolute;
-    top: 5.7rem;
-    right: 1rem;
-    z-index: 1000;
-    width: 100%;
-    height: 2rem;
-    .loading-title {
-      position: relative;
-      padding-top: 0.2rem;
-      margin-right: 1rem;
-      font-size: 1.5rem;
-      color: ${(props) => props.theme.color.gray400};
-      text-align: center;
-      background: linear-gradient(
-        to right,
-        #ff0000 5%,
-        #fff 20%,
-        #fff 80%,
-        #ff2200 95%
-      );
-      background-clip: text;
-      background-size: 200% auto;
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-      animation: ${opacityKeyframes} 2s linear infinite;
-    }
 
-    .loading-ball {
-      border: 0.5rem solid transparent;
-      background-image: linear-gradient(#fff, #fff),
-        linear-gradient(to right, white 0%, red 100%);
-      background-origin: border-box;
-      background-clip: content-box, border-box;
-      width: 1rem;
-      height: 1rem;
-      border-radius: 100%;
-      animation: ${loadingKeyframes} 2s linear infinite;
-    }
-  }
   .toastui-editor-defaultUI {
     .ProseMirror {
       font-size: 2.5rem;
@@ -102,21 +44,11 @@ interface IEditorContainerProps {
 }
 
 function EditorContainer({ initialValue, reference }: IEditorContainerProps) {
-  const { mutate, isLoading } = useMutation<
-    { data: string },
-    AxiosError,
-    FormData
-  >(async (body) => await api.postMultiPartData("/api/post-image", body));
-
+  const { mutate: imageUpload, isLoading } = useImageUpload();
   return (
     <>
       <Wrapper>
-        {isLoading && (
-          <span className="loading">
-            <span className="loading-title">이미지를 서버에 올리고 있어요</span>
-            <span className="loading-ball"></span>
-          </span>
-        )}
+        {isLoading && <SpiningLoading message="이미지를 저장하고 있어요!" />}
         <Editor
           ref={reference}
           customHTMLRenderer={{
@@ -150,9 +82,9 @@ function EditorContainer({ initialValue, reference }: IEditorContainerProps) {
           hooks={{
             addImageBlobHook: async (blob: Blob | File, callback) => {
               const formData = new FormData();
-              formData.append("data", blob);
+              formData.append("image", blob);
 
-              mutate(formData, {
+              imageUpload(formData, {
                 onSuccess: (response) => {
                   callback(
                     response.data,
