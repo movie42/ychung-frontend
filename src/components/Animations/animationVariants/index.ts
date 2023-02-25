6e28a08c8528a92bde9b44ce910cc75646f29099
movie42 <movie3342@gmail.com>
2023-02-25 17:22:55 +0900

    Apply .gitignore

--- src/components/Animations/animationVariants/index.ts
+++
@@ -1,3 +0,0 @@
-export { movingCard, opacity } from "./modalAnimation";
-export { navAnimationVariants } from "./navigationAnimationVariants";
-export { scrollAnimationVariants } from "./scrollAnimationVariants";
--- src/components/Canvases/Canvas.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type ICanvasProps = React.CanvasHTMLAttributes<HTMLCanvasElement>;
-
-const Canvas = (props: ICanvasProps) => {
-  return <canvas {...props}></canvas>;
-};
-
-export default Canvas;
